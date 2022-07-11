6993b219308c94bf8920a8b6ba35911fa219e9e5
filmmer428 <108912706+filmmer428@users.noreply.github.com>
2022-07-11 21:59:38 +0700

    update

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("Hellos")
+"""
+คำสั่งนี้ใช้ในการแสดงผลข้อความ
+และรองแสดงได้ทุกชนิด
+นะครับทุกคน
+"""
+#print("Hellos")
+print(-1)
+print(0)
+print("1"+"9")
+print(3.99)
+print(True)
+print(False)
+print("kongruksiam \n"+"studio")
